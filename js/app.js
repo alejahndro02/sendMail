8c7f6b1922786918ba165215f59407ea21f03ac8
Alejahndro <alejahndro02@gmail.com>
2020-03-29 22:26:38 -0600

    Creacion de las funciones para validar que los input no esten vacios

--- js/app.js
+++ js/app.js
@@ -8,11 +8,34 @@ const btnSend =document.getElementById('enviar');
 loadEventListeners();
 function loadEventListeners(){
     document.addEventListener('DOMContentLoaded', initApp);
-
+    email.addEventListener('blur', validate)
+    issue.addEventListener('blur', validate)
+    message.addEventListener('blur', validate)
 }
 
 //Funciones
-
 function initApp (){
-btnSend.disabled=true;
+    btnSend.disabled=true;
+}
+function validate(){
+    validateLength(this);
+    let errors =document.getElementsByClassName('error');
+    if(email.value !== '' && asunto.value !== '' && mensaje.value!==''){
+        if (errors.length === 0 ){
+            btnSend.disabled=false;
+        }
+    }else{
+        btnSend.disabled=true;
+    }
+}
+function validateLength(campo){
+    console.log(campo.value.length);
+    
+    if (campo.value.length > 0){
+        campo.style.borderBottomColor="green";
+        campo.classList.remove('error');
+    }else{
+        campo.style.borderBottomColor="red";
+        campo.classList.add('error');
+    }
 }
